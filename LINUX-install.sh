ac28d18c8c823e22b7e84de0537306e3e437d8b4
GieniuG <bartoszlwowski@gmail.com>
2024-03-12 18:11:04 +0100

    LINUX scripts

--- LINUX-install.sh
+++ LINUX-install.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# remove following "#" if you want to compile
-# if ! command -v npm &> /dev/null
-# then
-#     echo "npm is not installed. Please install Node.js, which includes npm, from https://nodejs.org/. And run 'npm install pkg'"
-#     exit 1
-# fi
-# 
-# pkg mangaScrape.js -t node18-linux-x64
+#if you don't have npm installed, install Node.js (https://nodejs.org/), which includes npm
+#run 'npx @puppeteer/browsers install chrome-headless-shell@stable' while in the same directory as mangaScrape.js
 
-mkdir $HOME/.mangaScrape/
-mkdir "$HOME/.mangaScrape/Downloaded Manga"
-cp chrome-headless-shell $HOME/.mangaScrape/
-mkdir $HOME/.config/mangaScrape/
-echo "{
- \"pathToDownloadDir\":\"$HOME/.mangaScrape/Downloaded Manga\",
- \"howManyTitles\":\"10\",
- \"doneMessage\":\"Everything done. Happy reading!\"
-}" > $HOME/.config/mangaScrape/config.json
-sudo cp mangaScrape /usr/local/bin
+echo "check that file for a moment!!"
+
+#remove that # from lines below only after doing things above
+
+#mkdir $HOME/.mangaScrape/
+#mkdir "$HOME/.mangaScrape/Downloaded Manga"
+#echo "[]" > "$HOME/.mangaScrape/Downloaded Manga/library.json"
+#cp -r chrome-headless-shell $HOME/.mangaScrape/
+#mkdir $HOME/.config/mangaScrape/
+#echo "{
+# \"pathToDownloadDir\":\"$HOME/.mangaScrape/Downloaded Manga\",
+# \"howManyTitles\":\"10\",
+# \"doneMessage\":\"Everything done. Happy reading!\"
+#}" > $HOME/.config/mangaScrape/config.json
+#sudo cp mangaScrape /usr/local/bin
+    
